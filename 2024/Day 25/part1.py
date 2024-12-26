fccf60cf471d992b89bb9695813e08e1d7c87fdd
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2024-12-25 18:43:31 -0600

    Update setup to get input

---
+++ 2024/Day 25/part1.py
@@ -0,0 +1,6 @@
+import os
+
+os.chdir(f"C:\Users\seanc\Documents\Advent-of-Code\\2024\\Day 25")
+
+txt = open("input.txt", "r")
+txt = txt.read()
--- setup_day.py
+++ setup_day.py
@@ -1,22 +1,24 @@
 import os
 import sys
+import requests
+import pathlib
 from datetime import date
-
-os.chdir("C:\\Users\\seanc\\source\\repos\\Advent-of-Code\\2024")
 
 days = []
 if len(sys.argv) == 1:
     days.append(date.today().day)
-
 else:
     for num in sys.argv[1:]:
         days.append(num)
 
+basePath = pathlib.Path(__file__).parent.resolve()
+year = 2024
+os.chdir(f"{basePath}\\{year}")
 
 for day in days:
     boilerplate = f"""import os
 
-os.chdir("C:\\\\Users\\\\seanc\\\\source\\\\repos\\\\Advent-of-Code\\\\2024\\\\Day {day}")
+os.chdir(f"{basePath}\\\\{year}\\\\Day {day}")
 
 txt = open("input.txt", "r")
 txt = txt.read()"""
@@ -28,4 +30,13 @@ txt = txt.read()"""
 
     two = open(f"Day {day}\\part2.py", "w")
     two.write(boilerplate)
-    open(f"Day {day}\\input.txt", "x")
+
+    input_file = open(f"Day {day}\\input.txt", "w")
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
+    cookie = open("..\\session.cookie").read().strip()
+    response = requests.get(url, {}, cookies={"session": cookie})
+    if not response.ok:
+        raise RuntimeError(
+            f"Request failed\n\tstatus code: {response.status_code}\n\tmessage: {response.content}"
+        )
+    input_file.write(response.text[:-1])
