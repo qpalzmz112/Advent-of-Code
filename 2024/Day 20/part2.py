4e2688048311c48c70b043e11d168e5414453de4
qpalzmz112 <68213464+qpalzmz112@users.noreply.github.com>
2025-01-18 17:36:42 -0600

    Another bad attempt

--- 2024/Day 20/part2.py
+++ 2024/Day 20/part2.py
@@ -24,64 +24,69 @@ def find_char(c):
 def in_bounds(pos):
     return pos[0] >= 0 and pos[0] < len(orig_txt) and pos[1] >= 0 and pos[1] < len(orig_txt[0])
 
-def get_cheats(pos, debug=False):
-    res = []
+def get_cheats(pos, threshold):
+    res = 0
 
     row_width = 1
-    for i in range(pos[1] - 20, pos[1] + 21):
+    for i in range(pos[0] - 20, pos[0] + 21):
         dx = row_width // 2
-        if debug:
-            padding = 41 - row_width // 2
-            row = " " * padding
 
-        for j in range(pos[0] - dx, pos[0] + dx + 1):
+        for j in range(pos[1] - dx, pos[1] + dx + 1):
             cheatEnd = (i, j)
-            if in_bounds(cheatEnd) and txt[cheatEnd] in ('.', 'E'):
-                res.append((cheatEnd, abs(pos[0] - i) + abs(pos[1] - j)))
-            if debug:
-                row += '.'
+            if in_bounds(cheatEnd) and txt[cheatEnd] != '#':
+                diff_x = abs(pos[0] - i)
+                diff_y = abs(pos[1] - j)
+                if diff_x + diff_y > 20:
+                        print(diff_x, diff_y)
+                        continue
+                if txt[pos] - txt[cheatEnd] - diff_x - diff_y >= threshold:
+                    #print(f"start: {pos}, txt[start]: {txt[pos]}, end: {cheatEnd}, txt[end]: {txt[cheatEnd]}, manhattan distance: {diff_x + diff_y}")
+                    res += 1
+                    
 
-        if debug:
-            print(row + " " * padding)
         row_width += 2 * (1 if i < pos[1] else -1)
 
     return res
 
-def solve():
-    dirs = {(-1, 0), (0, 1), (1, 0), (0, -1)}
-    cheats = defaultdict(list)
-    start = find_char('S')
-
+def preprocess(dirs):
+    end = find_char('E')
     todo = deque()
-    todo.append((start, 0))
+    todo.append((end, 0))
+
     while todo:
         pos, picoseconds = todo.pop()
-        if pos in cheats:
-            cheats[pos] = list(map(lambda x: picoseconds - x, cheats[pos]))
-
-        if txt[pos] == 'E':
-            return cheats
-        txt[pos] = 'O'        
+        txt[pos] = picoseconds      
 
         for d in dirs:
             newPos = (pos[0] + d[0], pos[1] + d[1])
-            if txt[newPos] in ('.', 'E'):
+            if txt[newPos] in('.', 'S'):
                 todo.append((newPos, picoseconds + 1))
                 break
 
-        for cheatEnd, length in get_cheats(pos, False):
-            cheats[cheatEnd].append(picoseconds + length)
+def solve(start, dirs, threshold):
+    res = 0
 
-def solution(debug=False):
-    cheats = solve()
-    if debug:
-        times = [time for cheat in cheats for time in cheats[cheat]]
-        counts = defaultdict(int)
-        for time in times:
-            counts[time] += 1
-        print(sorted(counts.items(), key=lambda x: x[0]))
-    return sum(1 if num >= 100 else 0 for val in cheats.values() for num in val)
+    todo = deque()
+    todo.append(start)
+    while todo:
+        pos = todo.pop()
+        res += get_cheats(pos, threshold)
 
+        if txt[pos] == 0:
+            return res     
+
+        for d in dirs:
+            newPos = (pos[0] + d[0], pos[1] + d[1])
+            if isinstance(txt[newPos], int) and txt[newPos] < txt[pos]:
+                todo.append(newPos)
+                break
+
+def solution():
+    start = find_char('S')
+    dirs = {(-1, 0), (0, 1), (1, 0), (0, -1)}
+    preprocess(dirs)
+    return solve(start, dirs, 100)
+    
 print(solution())
 # consider all possible values for cheatEnd at each pos - a cheatEnd can be at most 20 manhattan distance (dx + dy) away from pos
 # 1601605 too high
