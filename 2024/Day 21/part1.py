ab760ad153e5807391d6e4f663d508c8071e8271
Sean Carlson <seancarlson112@gmail.com>
2025-01-21 19:10:43 -0600

    need to optimize

--- 2024/Day 21/part1.py
+++ 2024/Day 21/part1.py
@@ -12,66 +12,32 @@ codes = txt.split()
 
 numpad = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3'], [None, '0', 'A']]
 dirpad = [[None, '^', 'A'], ['<', 'v', '>']]
-def in_bounds(arr, pos):
-    return pos[0] >= 0 and pos[1] >= 0 and pos[0] < len(arr) and pos[1] < len(arr[0])
 
-def count_switches(path):
-    res = 0
-    curr_key = path[0]
-    for key in path[1:]:
-        if key != curr_key:
-            curr_key = key
-            res += 1
-    return res
-
-def get_shortest_path(start, dest, keypad):
-    dirs = {(-1, 0): '^', (1, 0): 'v', (0, -1): '<', (0, 1): '>'}
-    todo = []
-    paths = []
-    for d in dirs:
-        pos = (start[0] + d[0], start[1] + d[1])
-        if in_bounds(keypad, pos) and pos is not None:
-            todo.append((pos, dirs[d], {start}))
-
-    while todo:
-        pos, moves, seen = todo.pop()
-        if pos == dest:
-            paths.append(moves)
-        if pos in seen:
-            continue
-
-        for d in dirs:
-            new_pos = (pos[0] + d[0], pos[1] + d[1])
-            if in_bounds(keypad, pos) and keypad[pos[0]][pos[1]] is not None:
-                new_seen = seen.copy()
-                new_seen.add(pos)
-                todo.append((new_pos, moves + dirs[d], new_seen))
-
-    min_length = min(len(path) for path in paths)
-    paths = [path for path in paths if len(path) == min_length]
-
-    paths = sorted(list(map(lambda x: (x, count_switches(x)), paths)), key = lambda x: x[1])
-    paths = [path for path, switches in paths if switches == paths[0][1]]
-    return paths[0]
-
-def populate_moves(keypad):
-    moves = defaultdict(defaultdict)
-
+def get_paths(keypad):
+    res = defaultdict(defaultdict)
     for row in range(len(keypad)):
         for col in range(len(keypad[0])):
-            curr_key = keypad[row][col]
-            if curr_key is None:
+            start = keypad[row][col]
+            if start is None:
                 continue
-
             for y in range(len(keypad)):
                 for x in range(len(keypad[0])):
-                    new_key = keypad[y][x]
-                    if new_key == curr_key or new_key is None:
+                    end = keypad[y][x]
+                    if end is None or end == start:
                         continue
 
-                    moves[curr_key][new_key] = get_shortest_path((row, col), (y, x), keypad) + 'A'
+                    horiz = abs(col - x) * ('<' if col > x else '>')
+                    vert = abs(row - y) * ('^' if row > y else 'v')
 
-    return moves
+                    # would be nice to understand this...
+                    if x > col and keypad[y][col] is not None:
+                        res[start][end] = vert + horiz + 'A'
+                    elif keypad[row][x] is not None:
+                        res[start][end] = horiz + vert + 'A'
+                    else:
+                        res[start][end] = vert + horiz + 'A'
+
+    return res
 
 def enter_code(code, numpad_moves, dirpad_moves, depth):
     if depth == 1:
@@ -94,10 +60,9 @@ def enter_code(code, numpad_moves, dirpad_moves, depth):
     return enter_code(moves, numpad_moves, dirpad_moves, depth - 1)
 
 def solution():
-    numpad_moves = populate_moves(numpad)
-    dirpad_moves = populate_moves(dirpad)
+    numpad_moves = get_paths(numpad)
+    dirpad_moves = get_paths(dirpad)
     
-    print(numpad_moves)
     res = 0
     for c in codes:
         num = int(c[:len(c) - 1])
@@ -107,5 +72,3 @@ def solution():
     return res
 
 print(solution())
-
-# 180874 too high
--- 2024/Day 21/part2.py
+++ 2024/Day 21/part2.py
@@ -1,8 +1,82 @@
 import os
 import pathlib
+from collections import defaultdict
 
 path = pathlib.Path(__file__).parent.resolve()
 os.chdir(path)
 
 txt = open("input.txt", "r")
 txt = txt.read()
+
+codes = txt.split()
+
+numpad = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3'], [None, '0', 'A']]
+dirpad = [[None, '^', 'A'], ['<', 'v', '>']]
+
+def get_paths(keypad):
+    res = defaultdict(defaultdict)
+    for row in range(len(keypad)):
+        for col in range(len(keypad[0])):
+            start = keypad[row][col]
+            if start is None:
+                continue
+            for y in range(len(keypad)):
+                for x in range(len(keypad[0])):
+                    end = keypad[y][x]
+                    if end is None or end == start:
+                        continue
+
+                    horiz = abs(col - x) * ('<' if col > x else '>')
+                    vert = abs(row - y) * ('^' if row > y else 'v')
+
+                    # would be nice to understand this...
+                    if x > col and keypad[y][col] is not None:
+                        res[start][end] = vert + horiz + 'A'
+                    elif keypad[row][x] is not None:
+                        res[start][end] = horiz + vert + 'A'
+                    else:
+                        res[start][end] = vert + horiz + 'A'
+
+    return res
+
+numpad_moves = get_paths(numpad)
+dirpad_moves = get_paths(dirpad)
+
+def enter_code(code):
+    moves = ""
+    i, j = -1, 0
+    use_numpad = code[1] in numpad_moves
+    while j + 1 < len(code):
+        i += 1
+        j += 1
+        if code[j] == code[i]:
+            moves += 'A'
+        elif use_numpad:
+            moves += numpad_moves[code[i]][code[j]]
+        else:
+            moves += dirpad_moves[code[i]][code[j]]
+    return moves
+
+def solution():  
+    res = 0
+    for c in codes:
+        print(c)
+        num = int(c[:len(c) - 1])   
+        
+        for i in range(26):
+            print(i)
+            j = 0
+            new_c = ""
+            while j < len(c):
+                if c[j] == 'A':
+                    new_c += enter_code('A' + c[:j + 1])
+                    c = c[j + 1:]
+                    j = 0
+                else:
+                   j += 1
+            c = new_c
+        length =  len(c)
+        res += length * num
+    return res
+
+print(solution())
